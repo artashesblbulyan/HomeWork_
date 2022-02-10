bd5eda60cdbb5da3bb99296f17664cca2a92ba96
Artash <artashesblbulyan@gmail.com>
2022-02-10 18:41:45 +0400

    home work 2_8 + 2_9

--- HomeWork_2_8.py
+++ HomeWork_2_8.py
@@ -2,6 +2,7 @@ import requests
 import os
 import pprint
 import json
+import threading
 import re
 
 
@@ -54,8 +55,10 @@ class UrlImage:
         try:
             for img_lit in self.reads_json_file():
                 if name in img_lit.keys():
-                    with open(f"{name}.jpg", "wb") as image_jpg:
-                        image_jpg.write(requests.get(img_lit[name]).content)
+                    with open(f"a//{name}.jpg", "wb") as image_jpg:
+                        # print(img_lit[name])
+                        if requests.get(img_lit[name]).status_code == 200:
+                            image_jpg.write(requests.get(img_lit[name]).content)
                 else:
                     continue
                     # return f"There is no image with that {name} in file.json"
@@ -66,18 +69,47 @@ class UrlImage:
         try:
             for img_lit in self.reads_json_file():
                 if name in img_lit.keys():
-                    with open(f"{name}.png", "wb") as image_png:
-                        image_png.write(requests.get(img_lit[name]).content)
+                    with open(f"a//{name}.png", "wb") as image_png:
+                        if requests.get(img_lit[name]).status_code == 200:
+                            image_png.write(requests.get(img_lit[name]).content)
                 else:
                     continue
                     # return f"There is no image with that {name} in file.json"
         except requests.exceptions.ConnectionError as err:
             return "ConnectionError"
 
+    def download_all_pictures(self):
+        thread_list = []
+        for i in self.reads_json_file():
+            for key, value in i.items():
+                # print(key, value)
+                x = threading.Thread(target=self.downloads_jpeg_image, args=(key,))
+                thread_list.append(x)
+                x.start()
+        for thread in thread_list:
+            thread.join()
+
 
 a = UrlImage("file.json")
-
 # print(a.reads_json_file())
-print(a.reads_file("https://edition.cnn.com/travel/article/wildlife-photographer-peoples-choice-intl-scli-scn/index.html"))
+# print(a.reads_file("https://edition.cnn.com/travel/article/wildlife-photographer-peoples-choice-intl-scli-scn/index.html"))
 # a.downloads_jpeg_image('dog')
+
+a.download_all_pictures()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
